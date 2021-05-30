11dae7cfff0406b0ab4728f6579334743a08659b
KIIIIK <2371066459@qq.com>
2021-05-30 22:37:54 +0800

    2021.05.30--AlexNet架构

---
+++ AlexNet.py
@@ -0,0 +1,36 @@
+import torch 
+from torch import nn  
+from d2l import torch as d2l 
+
+net = nn.Sequential(
+    nn.Conv2d(1, 96, kernel_size=11, stride=4, padding1=1), nn.ReLU(),
+    nn.MaxPool2d(kernel_size=3, stride=2),  
+    nn.Conv2d(96, 256, kernel_size=5, padding=2), nn.ReLU(),
+    nn.MaxPool2d(kernel_size=3, stride=2), 
+    nn.Conv2d(256, 384, kernel_size=3, padding=1), nn.ReLU(),
+    nn.Conv2d(384, 384, kernel_size=3, padding=1), nn.ReLU(),
+    nn.Conv2d(384, 256, kernel_size=3, padding=1), nn.ReLU(),
+    nn.MaxPool2d(kernel_size=3, stride=2), nn.Flatten(),
+    nn.Linear(6400, 4096), nn.ReLU(), nn.Dropout(p=0.5),  
+    nn.Linear(4096, 4096), nn.ReLU(), nn.Dropout(p=0.5),  
+    nn.Linear(4096, 10) 
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
